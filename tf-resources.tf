46adf60aa1695cdc5d01dbaed9ac05d369cc00b4
unknown <gauravkhatwani14@gmail.com>
2022-12-19 14:15:26 +0530

    second commit

--- tf-resources.tf
+++ tf-resources.tf
@@ -1,9 +1,10 @@
+# Creating EC2 Instance using TF Cloud and GitHub Integration
 resource "aws_instance" "web-server" {
   ami           = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
   tags          = {
     
-    "Name" = "Gaurav-EC2Instance"
+    "Name" = "Gaurav-EC2Instance-TFCloud-GitHub"
   }
 }
 
